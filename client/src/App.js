63bb9eaf1ec05f2fa99a5356fcb1c1203faa79ca
scarletMaster <nadinebenarbia@gmail.com>
2022-11-26 04:32:11 +0100

    navbar+heroSection

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,12 @@
 import React, { useState } from "react";
-import ConnectModal from "./components/ConnectModal";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from "./utils/firebase.config";
-import CreatePost from "./components/CreatePost";
+import Navbar from "./components/Navbar/Navbar";
+import Herosection from "./components/HeroSection/Herosection";
 
 const App = () => {
-  const [user, setUser] = useState(null);
-
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
-  });
-
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
-
   return (
     <div>
-      <div className="app-header">
-        {user && (
-          <div className="user-infos">
-            <span>{user?.displayName[0]}</span>
-            <h4>{user?.displayName}</h4>
-            <button onClick={() => handleLogout()}>
-              <i className="fa-solid fa-arrow-right-from-bracket"></i>
-            </button>
-          </div>
-        )}
-
-        {user ? <CreatePost /> : <ConnectModal />}
-      </div>
-      <div className="posts-container"></div>
+      <Navbar />
+      <Herosection />
     </div>
   );
 };
